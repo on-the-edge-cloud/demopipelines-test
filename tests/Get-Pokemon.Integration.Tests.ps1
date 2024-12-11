f6a6811168bcf6a3f2660b44b69dec5473d9e49c
Tomás Novau Foresi <tnovau@ontheedge.cloud>
2024-12-11 18:39:40 +0100

    Testing code coverage

--- tests/Get-Pokemon.Integration.Tests.ps1
+++ tests/Get-Pokemon.Integration.Tests.ps1
@@ -1,15 +1,15 @@
 BeforeAll {
-	. $PSScriptRoot/../src/Get-Pokemon.ps1
+    . $PSScriptRoot/../src/Get-Pokemon.ps1
 }
 
 Describe 'Get-Pokemon' {
     It 'Given no parameters, it should return pikachu' -Tag 'Integration' {
         $pikachu = Get-Pokemon
-		$pikachu.name | Should -Be 'pikachu'
+        $pikachu.name | Should -Be 'pikachu'
     }
 
-	It 'Given no parameters, it should return pikachu' -Tag 'Integration' {
-        $pikachu = Get-Pokemon -pokemonName 'ditto'
-		$pikachu.name | Should -Be 'ditto'
-    }
+    # It 'Given no parameters, it should return pikachu' -Tag 'Integration' {
+    #     $pikachu = Get-Pokemon -pokemonName 'ditto'
+    #     $pikachu.name | Should -Be 'ditto'
+    # }
 }
